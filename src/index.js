d786a23c72de0ef48d75062c516bc7362e137f62
Alicia Thomas <alicia@ondibs.com>
2022-11-03 15:08:43 -0400

    I think prettier/eslint are working now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from 'store';
-import { RouterProvider } from 'react-router-dom';
+import {
+	Provider
+} from 'react-redux';
+import {
+	store
+} from 'store';
+import {
+	RouterProvider
+} from 'react-router-dom';
 
-import router from 'routes/index';
+import router from './routes/index';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
 	<React.StrictMode>
 		<Provider store={store}>
 			<RouterProvider router={router}>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import {
 	createBrowserRouter
 } from 'react-router-dom';
-import App from 'App';
+import App from '../App';
 import SchedulePage from 'views/schedule';
 import Root from './root';
 
 // views
-
-const router = createBrowserRouter([{
-		path: '/',
-		element: < App / >
-	},
-	{
-		path: '/schedule',
-		element: < SchedulePage / >
-	}
-]);
+const router = createBrowserRouter({
+	path: '/',
+	element: < Root / > ,
+}, {
+	path: 'schedule',
+	element: < SchedulePage / > ,
+});
 
 export default router;
