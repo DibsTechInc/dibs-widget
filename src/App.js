1687add40f5c139e5a71b20270f389d07aee3823
Alicia Thomas <alicia@ondibs.com>
2022-11-03 00:25:52 -0400

    Data is being pulled from back end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 function App() {
-	const postUrl = process.env.REACT_APP_BASE_URL;
-	console.log(`postUrl: ${postUrl}`);
+	const baseURL = process.env.REACT_APP_BASE_URL;
+	console.log(`baseURL: ${baseURL}`);
+	React.useEffect(() => {
+		const bodytosend = {
+			dibsStudioId: 218,
+			timeZone: 'America/New_York',
+		};
+		const url = `${baseURL}/widget/get-schedule`;
+		const getSchedule = async () => {
+			await axios
+				.post(url, bodytosend)
+				.then((response) => {
+					console.log(JSON.stringify(response.data, null, 2));
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		};
+		getSchedule();
+	}, []);
 	return <div>Hello World 3!</div>;
 }
 
