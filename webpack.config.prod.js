1b731397678005126041efc23055302dd35bc81e
Alicia Thomas <alicia@ondibs.com>
2022-11-03 21:59:25 -0400

    more config for production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -40,7 +40,7 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    MiniCssExtractPlugin.loader,
                     'css-loader'
                 ]
             }
